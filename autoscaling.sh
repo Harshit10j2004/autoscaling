69d2a5bcf04f21407f21bec9c17d7b52daa2de47
Harshit gupta <onxy.harsh123@gmail.com>
2025-08-26 21:32:26 +0530

    added file lock and clear issue of multiple script running parallel version 1 almost done

--- autoscaling.sh
+++ autoscaling.sh
@@ -1,135 +1,144 @@
 #!/bin/bash
 
 set -x
+lockfile="/tmp/auto.lock"
 
-source /home/ubuntu/data/data.env
+exec 200>$lockfile
+(
+        echo "script id : $$" >> /home/ubuntu/logs/as_log.txt
 
-path="/home/ubuntu/data/instance_id.txt"
-log="/home/ubuntu/logs/as_log.txt"
+        flock -n 200 || {
 
-count=1
-
-cpu=$(mpstat 1 1 | grep Average | awk '{print 100 - int($12)}')
-
-echo "$(date)"
-echo "${cpu}"
-echo "hitpoint1"
+                echo "hitpoint s $(date)"
+                echo "script is lock by diff process $(date) " >> /home/ubuntu/logs/as_log.txt
+                exit 1
+        }
 
 
-if [ "${cpu}" -gt 70 ]; then
+        source /home/ubuntu/data/data.env
 
-        echo "hitpoint2 $(date)"
+        path="/home/ubuntu/data/instance_id.txt"
+        log="/home/ubuntu/logs/as_log.txt"
 
-        instance=$(aws ec2 run-instances \
-                --image-id $AMI \
-                --count "${count}" \
-                --instance-type $INSTANCE \
-                --key-name $KEY \
-                --security-group-ids $SECURITY \
-                --query Instances[0].InstanceId \
-                --output text)
-
-        aws ec2 wait instance-status-ok --instance-ids "${instance}"
-
-        echo "${instance}" >> "${path}"
-        echo "${instance} is created $(date)" >> "${log}"
-
-        echo "hitpoint3 $(date)"
-
-        aws elbv2 register-targets \
-                --target-group-arn $ARN \
-                --targets Id="${instance}"
-
+        echo "script is locked $(date) " >> "${log}"
         count=1
-        max_try=6
 
-        while true; do
+        cpu=$(mpstat 1 1 | grep Average | awk '{print 100 - int($12)}')
 
-                echo "hitpoint4 $(date)"
+        echo "$(date)"
+        echo "${cpu}"
+        echo "hitpoint1"
 
 
-                health=$(aws elbv2 describe-target-health \
-                        --target-group-arn $ARN \
-                        --targets Id="${instance}" \
-                        --query 'TargetHealthDescriptions[*].TargetHealth.State' \
+        if [ "${cpu}" -gt 70 ]; then
+
+                echo "hitpoint2 $(date)"
+
+                instance=$(aws ec2 run-instances \
+                        --image-id $AMI \
+                        --count "${count}" \
+                        --instance-type $INSTANCE \
+                        --key-name $KEY \
+                        --security-group-ids $SECURITY \
+                        --query Instances[0].InstanceId \
                         --output text)
-                echo " $health $(date) "
 
-                if [ "$count" -gt "$max_try" ]; then
+                aws ec2 wait instance-status-ok --instance-ids "${instance}"
 
-                        echo "loops end here"
+                echo "${instance}" >> "${path}"
+                echo "${instance} is created $(date)" >> "${log}"
 
-                        echo "hitpointn"
-                        break
+                echo "hitpoint3 $(date)"
 
-                fi
+                aws elbv2 register-targets \
+                        --target-group-arn $ARN \
+                        --targets Id="${instance}"
 
-                if [ "$health" == "healthy" ]; then
+                sleep 30
 
-                        echo "The instance is healthy $(date)" >> "${log}"
-                        echo "hitpoint5"
-                        break
+                for i in {1..15}; do
 
-                elif [ "$health" == "initial" ]; then
-
-                        echo "the instance is added to group $(date)" >> "${log}"
-                        echo "hitpoint6"
-                        sleep 10
+                        echo "hitpoint4 $(date)"
 
 
-                elif [ "$health" == "unhealthy" ]; then
-
-                        echo "target is still unhealthy after ${count} times checking" >> "${log}"
-
-                        (( count++ ))
-
-                        sleep 10
-                        echo "hitpoint7"
+                        health=$(aws elbv2 describe-target-health \
+                                --target-group-arn $ARN \
+                                --targets Id="${instance}" \
+                                --query 'TargetHealthDescriptions[*].TargetHealth.State' \
+                                --output text)
+                        echo " $health $(date) "
 
 
 
-                fi
+                        if [ "$health" == "healthy" ]; then
+
+                                echo "The instance is healthy $(date)" >> "${log}"
+                                echo "hitpoint5"
+                                break
+
+                        elif [ "$health" == "initial" ]; then
+
+                                echo "the instance is added to group $(date)" >> "${log}"
+                                echo "hitpoint6"
+                                sleep 10
 
 
-        done
+                        elif [ "$health" == "unhealthy" ]; then
 
-        echo "hitpoint8"
-
-fi
-
-echo "hitpoint9"
-
-if [ $cpu -lt 30 ]; then
-
-        echo "hitpoint6"
-
-        lines=$(wc -l < "${path}")
+                                echo "target is still unhealthy after ${i} times checking" >> "${log}"
 
 
-        if [ $lines -eq 0 ]; then
 
-                echo "there is no extra server is there"
+                                sleep 10
+                                echo "hitpoint7"
 
-                exit 0
+
+
+                        fi
+
+                        sleep 40
+
+                done
+
+                echo "hitpoint8"
+
         fi
 
-        echo "hitpoint x"
+        echo "hitpoint9"
 
-        instance_rm=$(tail -n 1 /home/ubuntu/data/instance_id.txt)
+        if [ $cpu -lt 30 ]; then
 
-        aws elbv2 deregister-targets \
-                --target-group-arn $ARN \
-                --targets Id="${instance_rm}"
+                echo "hitpoint6"
 
-        echo "hitpoint7"
-        echo "${instance_rm} is removed from the target group $(date)" >> "${log}"
+                lines=$(wc -l < "${path}")
 
-        aws ec2 terminate-instances --instance-ids "${instance_rm}"
 
-        echo "${instance_rm} is terminated $(date)" >> "${log}"
+                if [ $lines -eq 0 ]; then
 
-        sed -i '$d' /home/ubuntu/data/instance_id.txt
+                        echo "there is no extra server is there"
 
-        echo "hitpoint8"
-fi
+                        exit 0
+                fi
+
+                echo "hitpoint x"
+
+                instance_rm=$(tail -n 1 /home/ubuntu/data/instance_id.txt)
+
+                aws elbv2 deregister-targets \
+                        --target-group-arn $ARN \
+                        --targets Id="${instance_rm}"
+
+                echo "hitpoint7"
+                echo "${instance_rm} is removed from the target group $(date)" >> "${log}"
+
+                aws ec2 terminate-instances --instance-ids "${instance_rm}"
+
+                echo "${instance_rm} is terminated $(date)" >> "${log}"
+
+                sed -i '$d' /home/ubuntu/data/instance_id.txt
+
+                echo "hitpoint8"
+        fi
+
+)
 
