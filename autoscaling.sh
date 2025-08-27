ed2dbce35cd35a75f87b01e5187490d2a3a4a991
Harshit gupta <onxy.harsh123@gmail.com>
2025-08-27 19:30:36 +0530

    better logging

--- autoscaling.sh
+++ autoscaling.sh
@@ -2,15 +2,16 @@
 
 set -x
 lockfile="/tmp/auto.lock"
+log="/home/ubuntu/logs/as_log.txt"
 
 exec 200>$lockfile
 (
-        echo "script id : $$" >> /home/ubuntu/logs/as_log.txt
+
 
         flock -n 200 || {
 
                 echo "hitpoint s $(date)"
-                echo "script is lock by diff process $(date) " >> /home/ubuntu/logs/as_log.txt
+                echo "script is lock by diff process $(date) " >> "${log}"
                 exit 1
         }
 
@@ -18,21 +19,15 @@ exec 200>$lockfile
         source /home/ubuntu/data/data.env
 
         path="/home/ubuntu/data/instance_id.txt"
-        log="/home/ubuntu/logs/as_log.txt"
 
         echo "script is locked $(date) " >> "${log}"
         count=1
 
         cpu=$(mpstat 1 1 | grep Average | awk '{print 100 - int($12)}')
 
-        echo "$(date)"
-        echo "${cpu}"
-        echo "hitpoint1"
-
-
         if [ "${cpu}" -gt 70 ]; then
 
-                echo "hitpoint2 $(date)"
+                echo "CPU is gt than 80% starting the server at $(date)" >> "${log}"
 
                 instance=$(aws ec2 run-instances \
                         --image-id $AMI \
@@ -46,19 +41,18 @@ exec 200>$lockfile
                 aws ec2 wait instance-status-ok --instance-ids "${instance}"
 
                 echo "${instance}" >> "${path}"
-                echo "${instance} is created $(date)" >> "${log}"
-
-                echo "hitpoint3 $(date)"
+                echo "${instance} instance is created at $(date)" >> "${log}"
 
                 aws elbv2 register-targets \
                         --target-group-arn $ARN \
                         --targets Id="${instance}"
 
+                echo " instance is connecting to the target group at $(date) " >> "${log}"
                 sleep 30
 
                 for i in {1..15}; do
 
-                        echo "hitpoint4 $(date)"
+                        echo " Started to check the instance health before trying to add to target group at $(date) " >> "${log}"
 
 
                         health=$(aws elbv2 describe-target-health \
@@ -66,20 +60,20 @@ exec 200>$lockfile
                                 --targets Id="${instance}" \
                                 --query 'TargetHealthDescriptions[*].TargetHealth.State' \
                                 --output text)
-                        echo " $health $(date) "
+                        echo " current health is ${health} at $(date) " >> "${log}"
 
 
 
                         if [ "$health" == "healthy" ]; then
 
-                                echo "The instance is healthy $(date)" >> "${log}"
-                                echo "hitpoint5"
+                                echo "The instance is now healthy $(date)" >> "${log}"
+
                                 break
 
                         elif [ "$health" == "initial" ]; then
 
-                                echo "the instance is added to group $(date)" >> "${log}"
-                                echo "hitpoint6"
+                                echo "the instance is still in initial health at $(date)" >> "${log}"
+
                                 sleep 10
 
 
@@ -87,40 +81,33 @@ exec 200>$lockfile
 
                                 echo "target is still unhealthy after ${i} times checking" >> "${log}"
 
-
-
                                 sleep 10
-                                echo "hitpoint7"
-
-
-
                         fi
 
                         sleep 40
 
                 done
 
-                echo "hitpoint8"
+                echo "Instance is attached to target group now at $(date) " >> "${log}"
 
         fi
 
-        echo "hitpoint9"
+
 
         if [ $cpu -lt 30 ]; then
 
-                echo "hitpoint6"
+                echo "CPU usage is lower than 30% at $(date) " >> "${log}"
 
                 lines=$(wc -l < "${path}")
 
 
                 if [ $lines -eq 0 ]; then
 
-                        echo "there is no extra server is there"
+                        echo "there is no autoscaling vms attached till yet at $(date) " >> "${log}"
 
                         exit 0
                 fi
 
-                echo "hitpoint x"
 
                 instance_rm=$(tail -n 1 /home/ubuntu/data/instance_id.txt)
 
@@ -128,16 +115,15 @@ exec 200>$lockfile
                         --target-group-arn $ARN \
                         --targets Id="${instance_rm}"
 
-                echo "hitpoint7"
-                echo "${instance_rm} is removed from the target group $(date)" >> "${log}"
+                echo "${instance_rm} inatance is removed from the target group at $(date)" >> "${log}"
 
                 aws ec2 terminate-instances --instance-ids "${instance_rm}"
 
-                echo "${instance_rm} is terminated $(date)" >> "${log}"
+                echo "${instance_rm} instance is terminated at $(date)" >> "${log}"
 
                 sed -i '$d' /home/ubuntu/data/instance_id.txt
 
-                echo "hitpoint8"
+
         fi
 
 )
